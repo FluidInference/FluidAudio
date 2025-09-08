de6938a81604a30632536a7a98085a87a690bc3c
Alex-Wengg <hanweng9@gmail.com>
2025-09-08 02:33:11 -0400

    kokoro introduction

--- Sources/FluidAudioCLI/main.swift
+++ Sources/FluidAudioCLI/main.swift
@@ -18,6 +18,7 @@ func printUsage() {
             fleurs-benchmark        Run multilingual ASR benchmark on FLEURS dataset
             transcribe              Transcribe audio file using streaming ASR
             multi-stream            Transcribe multiple audio files in parallel
+            tts                     Synthesize speech from text using Kokoro TTS
             download                Download evaluation datasets
             help                    Show this help message
 
@@ -35,6 +36,8 @@ func printUsage() {
             fluidaudio transcribe audio.wav --low-latency
 
             fluidaudio multi-stream audio1.wav audio2.wav
+            
+            fluidaudio tts "Hello world" --output hello.wav
 
             fluidaudio download --dataset ami-sdm
         """
@@ -85,6 +88,13 @@ Task {
             await MultiStreamCommand.run(arguments: Array(arguments.dropFirst(2)))
         } else {
             print("Multi-stream requires macOS 13.0 or later")
+            exit(1)
+        }
+    case "tts":
+        if #available(macOS 13.0, *) {
+            await TTSCommand.run(arguments: Array(arguments.dropFirst(2)))
+        } else {
+            print("TTS requires macOS 13.0 or later")
             exit(1)
         }
     case "diarization-benchmark":
