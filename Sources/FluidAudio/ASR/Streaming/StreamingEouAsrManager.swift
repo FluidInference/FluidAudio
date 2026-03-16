2e11a7ac7e5500e7bd04409a96f3b2e1cd760adc
Benjamin Lee <benjaminlee314@icloud.com>
2026-03-15 19:54:55 -0700

    formatting

--- Sources/FluidAudio/ASR/Streaming/StreamingEouAsrManager.swift
+++ Sources/FluidAudio/ASR/Streaming/StreamingEouAsrManager.swift
@@ -323,7 +323,8 @@ public actor StreamingEouAsrManager {
         let applicationSupportURL = FileManager.default.urls(
             for: .applicationSupportDirectory, in: .userDomainMask
         ).first!
-        return applicationSupportURL
+        return
+            applicationSupportURL
             .appendingPathComponent("FluidAudio", isDirectory: true)
             .appendingPathComponent("Models", isDirectory: true)
             .appendingPathComponent("parakeet-eou-streaming", isDirectory: true)
