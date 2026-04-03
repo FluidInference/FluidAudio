82a3a09c0cc35fc1c6cea8b132fcf397b3f54cfe
Alex-Wengg <hanweng9@gmail.com>
2026-04-02 21:55:52 -0400

    Fix non-exhaustive switch: Add .ctcZhCn case to AsrManager decoder selection
    - Add .ctcZhCn to .v3 case in decoder selection switch
    - CTC zh-CN models use TdtDecoderV3 like v3 models
    - Fixes build failure in CI

--- Sources/FluidAudio/ASR/Parakeet/AsrManager.swift
+++ Sources/FluidAudio/ASR/Parakeet/AsrManager.swift
@@ -280,7 +280,7 @@ public actor AsrManager {
                 isLastChunk: isLastChunk,
                 globalFrameOffset: globalFrameOffset
             )
-        case .v3:
+        case .v3, .ctcZhCn:
             let decoder = TdtDecoderV3(config: adaptedConfig)
             return try await decoder.decodeWithTimings(
                 encoderOutput: encoderOutput,
